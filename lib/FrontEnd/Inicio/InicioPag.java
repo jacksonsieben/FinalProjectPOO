b77d94b0342712aa7755f3b3a5ec8e7d00e83444
sieben3105 <jackson_sieben@live.com>
2021-12-02 17:01:54 -0300

    finalizado

--- lib/FrontEnd/Inicio/InicioPag.java
+++ lib/FrontEnd/Inicio/InicioPag.java
@@ -4,7 +4,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
+import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
